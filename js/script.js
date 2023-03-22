837edac9687e6c22d126af86d8f182572117a56f
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-03-22 17:13:47 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bonnie Z
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function CalculateClicked() {
+  // input
+  const Height = parseInt(document.getElementById('Height').value)
+  const Base = parseInt(document.getElementById('Base').value)
+
+  // process
+  const area = (Height * Base) / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
